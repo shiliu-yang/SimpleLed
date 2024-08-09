656df60c2bcc6cc1e7a292887e941429f6795452
YangJie <yangjie6759@163.com>
2024-08-09 11:20:22 +0800

    Improve code and comments

--- examples/SimpleLed/SimpleLed.ino
+++ examples/SimpleLed/SimpleLed.ino
@@ -5,29 +5,46 @@
 // Initialise the LED and light it up when the pin output is high
 SimpleLed led(LED_PIN, HIGH);
 
+uint8_t curLedState = 0;
+
 void setup() {
+  Serial.begin(115200);
+
   // Trun on LED
-  led.on();
-  delay(3000);
+  led.setState(SimpleLed::LedState::ON);
+  curLedState = led.getState();
+  Serial.print("---> curLedState: "); Serial.println(curLedState);
+  delay(1500);
 
   // Trun off LED
-  led.off();
-  delay(3000);
+  led.setState(SimpleLed::LedState::OFF);
+  curLedState = led.getState();
+  Serial.print("---> curLedState: "); Serial.println(curLedState);
+  delay(1500);
 
   // Trun on LED
   led.setState(1);
-  delay(3000);
+  delay(1500);
 
   // Trun off LED
   led.setState(0);
-  delay(3000);
+  delay(1500);
 
-  // LED blinking, status switching every 500ms
-  led.blink(500);
+  // Trun on LED
+  led.on();
+  delay(1500);
+
+  // Trun off LED
+  led.off();
+  delay(1500);
+
+  // LED blinking, status switching every 100ms
+  led.blink(100);
 }
 
 void loop() {
   led.update();
-
   // Do other things...
+
+  delay(1);
 }
